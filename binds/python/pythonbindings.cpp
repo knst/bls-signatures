6b8ae19a8873b831a94669523d49653c1f37ccb2
Konstantin Akimov <github@knstqq.com>
2026-09-15 14:23:31 +0700

    binds: restore G1Element/G2Element multiplication by PrivateKey in Python
    test_unit.py exercises g1 * sk, sk * g1 and g2 * sk; the operator
    overloads exist in C++, only the pybind11 definitions had been dropped.

--- binds/python/pythonbindings.cpp
+++ binds/python/pythonbindings.cpp
@@ -518,6 +518,20 @@ PYBIND11_MODULE(dashbls, m)
             },
             py::is_operator())
         .def(
+            "__mul__",
+            [](G1Element &self, const PrivateKey &other) {
+                RelicGuard guard;
+                return self * other;
+            },
+            py::is_operator())
+        .def(
+            "__rmul__",
+            [](G1Element &self, const PrivateKey &other) {
+                RelicGuard guard;
+                return other * self;
+            },
+            py::is_operator())
+        .def(
             "__and__",
             [](G1Element &self, G2Element &other) {
                 RelicGuard guard;
@@ -627,6 +641,20 @@ PYBIND11_MODULE(dashbls, m)
             [](G2Element &self, G2Element &other) {
                 RelicGuard guard;
                 return self + other;
+            },
+            py::is_operator())
+        .def(
+            "__mul__",
+            [](G2Element &self, const PrivateKey &other) {
+                RelicGuard guard;
+                return self * other;
+            },
+            py::is_operator())
+        .def(
+            "__rmul__",
+            [](G2Element &self, const PrivateKey &other) {
+                RelicGuard guard;
+                return other * self;
             },
             py::is_operator())
         .def(
